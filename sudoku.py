744741ad3c41a59d05f9560c891e49b65838ebae
brunabazaluk <bruna@opal.localdomain>
2018-10-29 13:47:06 +0000

    relatorio added
    Signed-off-by: brunabazaluk <bruna@opal.localdomain>

--- sudoku.py
+++ sudoku.py
@@ -78,21 +78,21 @@ while sudoku != '':
     solution = solver.solve(cnf)
 
     if solution.success:
-	print("Solution %d:\n" % n)
-    	    for i in range(4):
-	        for j in range(4):
-		    for k in range(4):
-			if solution[x[i][j][k]]:
-			    sol[i][j] = k
+        print("Solution %d:\n" % n)
+            for i in range(4):
+                for j in range(4):
+                    for k in range(4):
+                        if solution[x[i][j][k]]:
+                            sol[i][j] = k
 
-	print("%d %d | %d %d" % (sol[0][0], sol[0][1], sol[0][2], sol[0][3]))
-	print("%d %d | %d %d" % (sol[1][0], sol[1][1], sol[1][2], sol[1][3]))
-	print("---------")
-	print("%d %d | %d %d" % (sol[2][0], sol[2][1], sol[2][2], sol[2][3]))
-	print("%d %d | %d %d\n" % (sol[3][0], sol[3][1], sol[3][2], sol[3][3]))
+        print("%d %d | %d %d" % (sol[0][0], sol[0][1], sol[0][2], sol[0][3]))
+        print("%d %d | %d %d" % (sol[1][0], sol[1][1], sol[1][2], sol[1][3]))
+        print("---------")
+        print("%d %d | %d %d" % (sol[2][0], sol[2][1], sol[2][2], sol[2][3]))
+        print("%d %d | %d %d\n" % (sol[3][0], sol[3][1], sol[3][2], sol[3][3]))
 
     else:
-	print("There is no solution.\n")
+        print("There is no solution.\n")
 
     n+=1
 
